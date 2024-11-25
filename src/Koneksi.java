d0eef94e582cb92047da0ea4d6c466d24cc9fd45
HaikalAditya <haikaladitya6@gmail.com>
2024-11-25 10:24:30 +0800

    selesai

--- src/Koneksi.java
+++ src/Koneksi.java
@@ -1,13 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author User
- */
 public class Koneksi {
-    
+    private static Connection mysqlkonek;
+
+    // Metode untuk mendapatkan koneksi ke database
+    public static Connection getConnection() throws SQLException {
+        if (mysqlkonek == null) {
+            try {
+                String DB = "jdbc:mysql://localhost:3306/nomor_telpon"; // URL database
+                String user = "root"; // Nama pengguna database
+                String pass = ""; // Kata sandi database
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver()); // Driver untuk MySQL
+                mysqlkonek = (Connection) DriverManager.getConnection(DB, user, pass);
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Koneksi gagal: " + e.getMessage());
+            }
+        }
+        return mysqlkonek;
+    }
+    // Metode main untuk pengujian koneksi
+    public static void main(String[] args) {
+        try {
+            getConnection();
+        } catch (SQLException e) {
+
+}
+    }
 }
